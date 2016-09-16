bb78ee23fc25f9ecd9510391db6b6f7dca46eb59
Kurt <Kurt@KurtSch.com.au>
2016-09-16 20:26:12 +1000

    Refactored PlistPArser to be more readable

--- Program/PListParser.java
+++ Program/PListParser.java
@@ -15,11 +15,92 @@ import org.w3c.dom.Node;
 import org.w3c.dom.Element;
 
 
-//TODO: try to look for code that is reused and methodise it
 public class PListParser {
 
+    private static ArrayList<String> aKeyItems = new ArrayList<>();
+    private static ArrayList<String> aStringItems = new ArrayList<>();
+    private static ArrayList<String> aOccurrenceArrayItems = new ArrayList<>();
+    private static Element eNormalCard;
 
-    public static ArrayList<BaseCard> getCardsList(){
+
+    private static ArrayList<String> elementsToArray(Element element, String elementName) {
+        ArrayList<String> tempArrayList = new ArrayList<>();
+        NodeList nodes = element.getElementsByTagName(elementName);
+        for (int j = 0; j < nodes.getLength(); j++) {
+            if (!nodes.item(j).getParentNode().getNodeName().equals("array")) {  ///make sure it is not the strings in the array
+                tempArrayList.add(nodes.item(j).getTextContent());
+            }
+        }
+        return tempArrayList;
+    }
+
+    private static BaseCard extractTrumpOrRulesCard() {
+        if (aKeyItems.get(3).toLowerCase().contains("trump")) {
+            //System.out.println("its a trump card");
+
+            return (new TrumpCard(aStringItems.get(0),
+                    aStringItems.get(1),
+                    aKeyItems.get(3),
+                    aStringItems.get(2),
+                    aStringItems.get(3)));
+
+        } else {
+            //System.out.println("its a rules card");
+
+            return (new RuleCard(aStringItems.get(0),
+                    aStringItems.get(1),
+                    aKeyItems.get(3),
+                    aStringItems.get(2),
+                    aStringItems.get(3)));
+        }
+
+    }
+
+    private static BaseCard extractMineralCard() {
+
+        NodeList nArrayNodes = eNormalCard.getElementsByTagName("array");
+        if (nArrayNodes.item(0).getNodeType() == Node.ELEMENT_NODE) {
+            Element eOccurrence = (Element) nArrayNodes.item(0);
+            aOccurrenceArrayItems = elementsToArray(eOccurrence, "string");
+        }
+
+        String[] aOccurrence = new String[aOccurrenceArrayItems.size()];
+        for (int i = 0; i <= aOccurrenceArrayItems.size() - 1; i++) {
+            aOccurrence[i] = aOccurrenceArrayItems.get(i);
+        }
+
+        String[] sHardness = aStringItems.get(6).split("-");
+        if (sHardness.length < 2) {
+            sHardness = aStringItems.get(6).split(" ");
+        }
+
+        Double[] dHardness = new Double[sHardness.length];
+        for (int i = 0; i <= sHardness.length - 1; i++) {
+            dHardness[i] = Double.parseDouble(sHardness[i]);
+        }
+
+        String[] sSpecificGravity = aStringItems.get(7).split("-");
+        Double[] dSpecificGravity = new Double[sSpecificGravity.length];
+        for (int i = 0; i <= sSpecificGravity.length - 1; i++) {
+            dSpecificGravity[i] = Double.parseDouble(sSpecificGravity[i]);
+        }
+
+        return new MineralCard(aStringItems.get(0),
+                aStringItems.get(1),
+                aKeyItems.get(3),
+                aStringItems.get(2),
+                aStringItems.get(3),
+                aStringItems.get(4),
+                aStringItems.get(5),
+                aOccurrence,
+                dHardness,
+                dSpecificGravity,
+                aStringItems.get(8),
+                aStringItems.get(9).trim(),
+                aStringItems.get(10));
+    }
+
+    public static ArrayList<BaseCard> getCardsList() {
         ArrayList<BaseCard> cardList = new ArrayList<>();
 
         try {
@@ -34,143 +115,41 @@ public class PListParser {
             NodeList nList = doc.getElementsByTagName("dict");
             for (int temp = 0; temp < nList.getLength(); temp++) {
                 Node nNode = nList.item(temp);
-                //System.out.println("\nDict Element :"
-                //+ nNode.getNodeName());
 
-                //get cards from root element
                 NodeList nChildNodes = nNode.getChildNodes();
-                //System.out.println(nChildNodes.getLength());
-
-                ArrayList<String> aKeyItems = new ArrayList<String>();
-                ArrayList<String> aStringItems = new ArrayList<String>();
-                ArrayList<String> aOccurrenceArrayItems = new ArrayList<String>();
-
-                ArrayList<Object> aWhatIWillMakeCardFrom = new ArrayList<>();
 
                 if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-                    Element eNormalCard = (Element) nNode;
+                    eNormalCard = (Element) nNode;
 
-                    //get all the keys
-                    //TODO: getKeys()
-                    NodeList nKeyNodes = eNormalCard.getElementsByTagName("key");
-                    for (int j = 0; j < nKeyNodes.getLength(); j++) {
-                        aKeyItems.add(nKeyNodes.item(j).getTextContent());
-                    }
-
-                    //get all the strings
-                    //TODO: getStrings()
-                    NodeList nStringNodes = eNormalCard.getElementsByTagName("string");
-                    for (int j = 0; j < nStringNodes.getLength(); j++) {
-                        if (!nStringNodes.item(j).getParentNode().getNodeName().equals("array")) {  ///make sure it is not the strings in the array
-                            aStringItems.add(nStringNodes.item(j).getTextContent());
-                        }
-                    }
-
-
+                    aKeyItems = elementsToArray(eNormalCard, "key");
+                    aStringItems = elementsToArray(eNormalCard, "string");
                     //check for playing card
                     if (nChildNodes.getLength() == 53) {
-                        //TODO: extractMineralCard();
-                        //System.out.println("its a normal card");
-
-                        //TODO: getArrayStrings()
-                        NodeList nArrayNodes = eNormalCard.getElementsByTagName("array");
-                        if (nArrayNodes.item(0).getNodeType() == Node.ELEMENT_NODE) {
-                            Element eOccurrence = (Element) nArrayNodes.item(0);
-                            NodeList nOccurrence = eOccurrence.getElementsByTagName("string");
-                            for (int j = 0; j < nOccurrence.getLength(); j++) {
-                                aOccurrenceArrayItems.add(nOccurrence.item(j).getTextContent());
-                            }
-
-                        }
-
-                        String[] aOccurrence = new String[aOccurrenceArrayItems.size()];
-                        for(int i = 0; i <= aOccurrenceArrayItems.size()-1; i++ ) {
-                            aOccurrence[i] = aOccurrenceArrayItems.get(i);
-                        }
-
-                        String[] sHardness = aStringItems.get(6).split("-");
-                        if (sHardness.length < 2 ){
-                            sHardness = aStringItems.get(6).split(" ");
-                        }
-
-                        Double[] dHardness = new Double[sHardness.length];
-                        for(int i = 0; i <= sHardness.length-1; i++ ) {
-                            dHardness[i] = Double.parseDouble(sHardness[i]);
-                        }
-
-                        String[] sSpecificGravity = aStringItems.get(7).split("-");
-                        Double[] dSpecificGravity = new Double[sSpecificGravity.length];
-                        for(int i = 0; i <= sSpecificGravity.length-1; i++ ) {
-                            dSpecificGravity[i] = Double.parseDouble(sSpecificGravity[i]);
-                        }
-
-                        MineralCard newMineralCard = new MineralCard(aStringItems.get(0),
-                                aStringItems.get(1),
-                                aKeyItems.get(3),
-                                aStringItems.get(2),
-                                aStringItems.get(3),
-                                aStringItems.get(4),
-                                aStringItems.get(5),
-                                aOccurrence,
-                                dHardness,
-                                dSpecificGravity,
-                                aStringItems.get(8),
-                                aStringItems.get(9).trim(),
-                                aStringItems.get(10) );
-
-                        cardList.add(newMineralCard);
+                        cardList.add(extractMineralCard());
                     }
-
+                    //Check for rules or trump card
                     if (nChildNodes.getLength() == 21) {
                         //System.out.println("its a trump or rules card");
-                        if (aKeyItems.get(3).toLowerCase().contains("trump")) {
-                            //TODO: extractTrumpCard()
-                            //System.out.println("its a trump card");
-
-                            TrumpCard trumpCard = new TrumpCard(aStringItems.get(0),
-                                    aStringItems.get(1),
-                                    aKeyItems.get(3),
-                                    aStringItems.get(2),
-                                    aStringItems.get(3));
-
-                            cardList.add(trumpCard);
-
-
-                        } else if (aKeyItems.get(3).toLowerCase().contains("rule")){
-                            //TODO: extractRulesCard()
-                            //System.out.println("its a rules card");
-
-                            RuleCard ruleCard = new RuleCard(aStringItems.get(0),
-                                    aStringItems.get(1),
-                                    aKeyItems.get(3),
-                                    aStringItems.get(2),
-                                    aStringItems.get(3));
-
-                            cardList.add(ruleCard);
-                        }
+                        cardList.add(extractTrumpOrRulesCard());
                     }
                 }
             }
         } catch (Exception e) {
+            //TODO: print the error to trace i.e. could not reads plist file...
             e.printStackTrace();
         }
-        return  cardList;
+        return cardList;
     }
 
     public static void main(String[] args) {
-
-
-
         try {
-
-
             ArrayList<BaseCard> cardList = PListParser.getCardsList();
 
-            for(BaseCard card : cardList) {
+            for (BaseCard card : cardList) {
 
                 System.out.println("Card Title is " + card.getTitle());
-                if(card.getError() > 0){
-                    System.out.println("There was at least one error with this card: " + card.getTitle() + " \n ErrorCount = " +card.getError());
+                if (card.getError() > 0) {
+                    System.out.println("There was at least one error with this card: " + card.getTitle() + " \n ErrorCount = " + card.getError());
                 }
             }
 
